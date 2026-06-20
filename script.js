2752c5a27ec4aafbfd0e2b209cdd5599bd410d71
Salim Ms <139252414+SalimMs-tech@users.noreply.github.com>
2026-06-20 09:53:01 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,1 +1,17 @@
+async function updateGold(){
 
+const url =
+"https://api.twelvedata.com/price?symbol=XAU/USD&apikey=c3b584cce80d43f08b1001504ed81afb";
+
+const response = await fetch(url);
+
+const data = await response.json();
+
+document.getElementById("gold-price").innerHTML =
+data.price;
+
+}
+
+updateGold();
+
+setInterval(updateGold,10000);
