766c3d59ad140b90855e4e53a51efe04523332a7
Salim Ms <139252414+SalimMs-tech@users.noreply.github.com>
2026-06-20 10:11:12 -0700

    Update script.js

--- script.js
+++ script.js
@@ -1,17 +1,56 @@
-async function updateGold(){
+const apiKey = "c3b584cce80d43f08b1001504ed81afb";
 
-const url =
-"https://api.twelvedata.com/price?symbol=XAU/USD&apikey=c3b584cce80d43f08b1001504ed81afb";
+async function updateMarket(){
 
-const response = await fetch(url);
+try {
+
+const response = await fetch(
+`https://api.twelvedata.com/price?symbol=XAU/USD&apikey=${apiKey}`
+);
 
 const data = await response.json();
 
+const price = Number(data.price);
+
+
 document.getElementById("gold-price").innerHTML =
-data.price;
+price.toFixed(2);
+
+
+// AI INSIGHT LOGIC
+
+let insight = "";
+
+if(price > 4000){
+
+insight =
+"Gold momentum is strong. Buyers are currently dominating the market.";
+
+}
+else{
+
+insight =
+"Gold is showing weakness. Market pressure may be increasing.";
 
 }
 
-updateGold();
 
-setInterval(updateGold,10000);
+document.getElementById("ai-insight").innerHTML =
+insight;
+
+
+
+}
+
+catch(error){
+
+console.log(error);
+
+}
+
+}
+
+
+updateMarket();
+
+setInterval(updateMarket,60000);
